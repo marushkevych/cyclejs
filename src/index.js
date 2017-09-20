18f78d2f120e79deabca994e4772c0e033c20795
Andrey Marushkevych <marushkevych@gmail.com>
2017-09-19 22:25:00 -0400

    implemented our own run() function and drivers

--- src/index.js
+++ src/index.js
@@ -1,19 +1,62 @@
-const choo = require('choo')
-const html = require('choo/html')
 
-const app = choo()
-
-const view = (state, prev, send) => {
-  return html`
-  <div class='tile'>
-    <h1>Hello World!</h1>
-  </div>
-  `
+function main(sources) {
+  
+  const click$ = sources.DOM
+  
+  const DOM = click$
+    .startWith(null)
+    .map(() => xs.periodic(1000).fold(prev => prev + 1, 0))
+    .flatten()
+    .map(i => `Seconds elapsed: ${i}`)  
+  
+  const log = click$
+    .map(() => `Restarted!`)  
+  
+  return {
+    DOM,
+    log
+  }
 }
 
-app.router([
-  ['/', view]
-])
+function domDriver(text$) {
+  text$.subscribe({
+    next: str => {
+      const elem = document.querySelector('#app')
+      elem.textContent = str
+    }
+  })
+  
+  const domSource = fromEvent(document, 'click')
+  return domSource
+}
 
-const tree = app.start()
-document.body.appendChild(tree)
+function logDriver(msg$) {
+  msg$.subscribe({
+    next: msg => console.log(msg)
+  })
+}
+
+
+function run(mainFn, drivers) {
+  const sources = {}
+  const fakeSinks = {}
+  
+  // call each driver with fake sink and gather sources
+  Object.entries(drivers).forEach(([key, driver]) => {
+    const fakeSink = xs.create()
+    fakeSinks[key] = fakeSink
+    sources[key] = driver(fakeSink)
+  }) 
+  
+  // get real sinks and
+  // replace each fake sink with real one
+  const sinks = mainFn(sources)
+  Object.entries(sinks).forEach(([key, sink]) => {
+    fakeSinks[key].imitate(sink)
+  })
+}
+
+run(main, {
+  DOM: domDriver,
+  log: logDriver
+})
