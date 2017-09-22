32cc6b5162531449a86c4ea1082e4f5ca51fcfbe
Andrey Marushkevych <marushkevych@gmail.com>
2017-09-22 01:20:00 -0400

    BMI calculator

--- src/index.js
+++ src/index.js
@@ -1,45 +1,50 @@
-const {div, h1, h4, a, p, button, makeDOMDriver} = CycleDOM
-const {makeHTTPDriver} = CycleHTTPDriver
+const {div, h2, label, p, button, input, makeDOMDriver} = CycleDOM
 
 function main({DOM, HTTP}) {
-  const click$ = DOM.select('.get-first').events('click')
-  
-  const responce$ = HTTP
-    .select('user-data')
-    .flatten()
-    .map(res => res.body)
-    .startWith({})
-  
-  const request$ = click$.map(ev => {
-    return {
-      url: 'https://jsonplaceholder.typicode.com/users/1',
-      method: 'GET',
-      category: 'user-data'
-    }
-  })
+  const changeWeight$ = DOM.select('.weight').events('input')
+  const changeHight$ = DOM.select('.height').events('input')
 
-  const vdom$ = responce$ .map(responce =>
+  const weight$ = changeWeight$.map(ev => ev.target.value).startWith(40)
+  const height$ = changeHight$.map(ev => ev.target.value).startWith(150)
+  
+  const state$ = xs.combine(weight$, height$)
+    .map(([weight, height]) => {
+      const hightMeters = height*0.01
+      const bmi = Math.round(weight/(hightMeters*hightMeters))
+      return {weight, height, bmi}
+    })
+
+  const vdom$ = state$.map(({weight, height, bmi}) =>
     div([
-      button('.get-first', 'Get first user'),
-      div('.user-details', [
-        h1('.user-name', responce.name),
-        h4('.user-email', responce.email),
-        a('.user-website', {
-          attrs: {href: responce.website}
-        }, responce.website)
-      ])
+      div([
+        label(`Weight: ${weight} kg`),
+        input('.weight', {attrs: {
+          type: 'range',
+          min: 40,
+          max: 150,
+          value: 40
+        }})
+      ]),
+      div([
+        label(`Hight: ${height} cm`),
+        input('.height', {attrs: {
+          type: 'range',
+          min: 150,
+          max: 220,
+          value: 150
+        }})
+      ]),
+      h2(`BMI is ${bmi}`)
     ])
   )
 
   return {
-    DOM: vdom$,
-    HTTP: request$
+    DOM: vdom$
   }
 }
 
 const drivers = {
-  DOM: makeDOMDriver('#app'),
-  HTTP: makeHTTPDriver()
+  DOM: makeDOMDriver('#app')
 }
 
 Cycle.run(main, drivers)
