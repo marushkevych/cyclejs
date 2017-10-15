1211300f4aeed302ab67c57f7eb76a78e9a445d4
Andrey Marushkevych <marushkevych@gmail.com>
2017-10-14 23:16:45 -0400

    passing props as sources to component

--- src/SliderComponent.js
+++ src/SliderComponent.js
@@ -1,44 +1,44 @@
 const {div, h2, label, p, button, input} = CycleDOM
-
-function SliderComponent(min, max, name, unit) {
   
-    function main({DOM}) {
-      return {
-        DOM: view(model(intent(DOM)))
-      }
-    }
-    
-    function intent(DOM) {
-      const sliderChange$ = DOM.select('.slider').events('input')
-    
-      const sliderValue$ = sliderChange$.map(ev => ev.target.value)
-    
-      return sliderValue$
-    }
-
-    function model(sliderValue$) {
-      return sliderValue$.startWith(min).map(value => {
-        return {value}
-      })
-    }
-    
-    function view(state$) {
-      const vdom$ = state$.map(({value}) =>
-        div([
-          label(`${name}: ${value} ${unit}`),
-          input('.slider', {attrs: {
-            type: 'range',
-            min,
-            max,
-            value
-          }})
-        ])
-      )
-    
-      return vdom$
-    }
-  
-    return main
+function sliderComponent({DOM, props}) {
+  const actions = intent(DOM)
+  const state$ = model(actions, props)
+  return {
+    DOM: view(state$)
   }
+}
 
-  module.exports = SliderComponent
+function intent(DOM) {
+  const sliderChange$ = DOM.select('.slider').events('input')
+
+  const sliderValue$ = sliderChange$.map(ev => ev.target.value)
+
+  return {sliderValue$}
+}
+
+function model(actions, props$) {
+  return props$.map(props => {
+    const {init, name, unit, min, max} = props
+    return actions.sliderValue$.startWith(init).map(value => {
+      return {value, name, unit, min, max}
+    })
+  }).flatten()
+}
+
+function view(state$) {
+  const vdom$ = state$.map(({value, name, unit, min, max}) =>
+    div([
+      label(`${name}: ${value} ${unit}`),
+      input('.slider', {attrs: {
+        type: 'range',
+        min,
+        max,
+        value
+      }})
+    ])
+  )
+
+  return vdom$
+}
+  
+module.exports = sliderComponent
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const isolate = require('@cycle/isolate').default
-const SliderComponent = require('./SliderComponent')
+const sliderComponent = require('./SliderComponent')
 const {div, makeDOMDriver} = CycleDOM
 
-const wightSlider = isolate(SliderComponent(40, 150, 'Weight', 'kg'), '.weight')
-const hightSlider = isolate(SliderComponent(140, 220, 'Height', 'cm'), '.hight')
+const wightSlider = isolate(sliderComponent, '.weight')
+const hightSlider = isolate(sliderComponent, '.hight')
 
 function main(sources) {
+  const weightProps$ = xs.of({
+    name: 'Weight',
+    unit: 'kg',
+    min: 40,
+    max: 150,
+    init: 70,
+  })
+  const weightSinks = wightSlider({DOM: sources.DOM, props: weightProps$})
 
-  const weightSinks = wightSlider(sources)
-  const heightSinks = hightSlider(sources)
+  const heightProps$ = xs.of({
+    name: 'Height',
+    unit: 'cm',
+    min: 140,
+    max: 220,
+    init: 170,
+  })
+  const heightSinks = hightSlider({DOM: sources.DOM, props: heightProps$})
 
   const vdom$ = xs.combine(weightSinks.DOM, heightSinks.DOM)
     .map(([weightVDOM, heightVDOM]) => 
